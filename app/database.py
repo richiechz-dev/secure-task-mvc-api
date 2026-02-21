d120b880943f1d04c9199516b4c51c6d30edb802
Ricardo Chávez <gic.rc455@gmail.com>
2026-02-21 13:44:06 -0600

    Updtae database.py with def get_db()
    To create a function for interact with the db

--- app/database.py
+++ app/database.py
@@ -17,4 +17,11 @@ class Base(DeclarativeBase): # La clase Base sirve para declarar todos los model
 engine = create_engine(sqlite_url, connect_arg = connect_arg)
 SessionLocal = sessionmaker(bind=engine)    #SessionLocal es un objeto fabrica, pues implementa el patron factory
 
+def get_db():
+    db = SessionLocal() #creamos una instancia de la clase SessionLocal, que es un objeto de sesión de SQLAlchemy. Esta instancia se utiliza para interactuar con la base de datos.
+    try:
+        yield db #yield es una palabra clave en Python que se utiliza para crear generadores. En este caso, get_db es una función generadora que produce una instancia de sesión de base de datos cada vez que se llama. Esto permite que la sesión se utilice en un contexto específico (por ejemplo, durante una solicitud HTTP) y luego se cierre automáticamente después de su uso.
+    finally:
+        db.close() #Finalmente, se cierra la sesión de base de datos para liberar recursos y evitar conexiones abiertas innecesarias.
+
 
