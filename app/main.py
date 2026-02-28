2b802b33d4ff7c8052f2e5c449b7239e6a2a16f2
Ricardo Chávez <gic.rc455@gmail.com>
2026-02-27 18:07:03 -0600

    Create DB tables on startup; add auth router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.controllers import ctrl_auth
+from app.database import Base, engine
+from app.models import task, token, user
+
+
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI(on_startup=[on_startup])
+app.include_router(ctrl_auth.router)
+
 
 @app.get("/")
 async def root():
